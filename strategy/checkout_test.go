d343b6b2682dd3ff8853bf4dd45a9a260dc64570
Denis Souza <denyssouza1@hotmail.com>
2025-07-16 12:16:15 -0300

    refatorando testes do pattern strategy

--- strategy/checkout_test.go
+++ strategy/checkout_test.go
@@ -1,6 +1,7 @@
 package strategy
 
 import (
+	"database/sql"
 	"denis-souzaa/design-patterns-go/config"
 	"fmt"
 	"log"
@@ -11,77 +12,80 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
-func TestMustCalculateFareAirport(t *testing.T) {
-	src := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(src)
-	plate := fmt.Sprintf("AAA%d", r.Intn(9000)+1000)
-	db, err := config.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
-	checkin := NewCheckin(parkingTicketRepo)
-	inputCheckin := Input{
-		plate:       plate,
-		checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
-		location:    "airport",
-	}
-	checkin.Execute(inputCheckin)
-	checkout := NewCheckout(parkingTicketRepo)
-	inputCheckout := InputCheckout{
-		plate:        plate,
-		checkoutDate: time.Date(2025, 07, 15, 12, 0, 0, 0, time.Local),
-	}
-	output := checkout.Execute(inputCheckout)
-	assert.Equal(t, float64(20), output.fare)
-}
+func TestMustCalculateFare(t *testing.T) {
 
-func TestMustCalculateFareShopping(t *testing.T) {
-	src := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(src)
-	plate := fmt.Sprintf("AAA%d", r.Intn(9000)+1000)
-	db, err := config.New()
-	if err != nil {
-		log.Fatal(err)
+	generatedCarPlate := func(t *testing.T) string {
+		t.Helper()
+		src := rand.NewSource(time.Now().UnixNano())
+		r := rand.New(src)
+		return fmt.Sprintf("AAA%d", r.Intn(9000)+1000)
 	}
-	parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
-	checkin := NewCheckin(parkingTicketRepo)
-	inputCheckin := Input{
-		plate:       plate,
-		checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
-		location:    "shopping",
-	}
-	checkin.Execute(inputCheckin)
-	checkout := NewCheckout(parkingTicketRepo)
-	inputCheckout := InputCheckout{
-		plate:        plate,
-		checkoutDate: time.Date(2025, 07, 15, 15, 0, 0, 0, time.Local),
-	}
-	output := checkout.Execute(inputCheckout)
-	assert.Equal(t, float64(30), output.fare)
-}
 
-func TestMustCalculateFareBeach(t *testing.T) {
-	src := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(src)
-	plate := fmt.Sprintf("AAA%d", r.Intn(9000)+1000)
-	db, err := config.New()
-	if err != nil {
-		log.Fatal(err)
+	connectionDb := func(t *testing.T) *sql.DB {
+		db, err := config.New()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return db
 	}
-	parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
-	checkin := NewCheckin(parkingTicketRepo)
-	inputCheckin := Input{
-		plate:       plate,
-		checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
-		location:    "beach",
-	}
-	checkin.Execute(inputCheckin)
-	checkout := NewCheckout(parkingTicketRepo)
-	inputCheckout := InputCheckout{
-		plate:        plate,
-		checkoutDate: time.Date(2025, 07, 15, 15, 0, 0, 0, time.Local),
-	}
-	output := checkout.Execute(inputCheckout)
-	assert.Equal(t, float64(10), output.fare)
+
+	t.Run("Deve calcular tarifa para aeroporto", func(t *testing.T) {
+		plate := generatedCarPlate(t)
+		db := connectionDb(t)
+		parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
+		checkin := NewCheckin(parkingTicketRepo)
+		inputCheckin := Input{
+			plate:       plate,
+			checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
+			location:    "airport",
+		}
+		checkin.Execute(inputCheckin)
+		checkout := NewCheckout(parkingTicketRepo)
+		inputCheckout := InputCheckout{
+			plate:        plate,
+			checkoutDate: time.Date(2025, 07, 15, 12, 0, 0, 0, time.Local),
+		}
+		output := checkout.Execute(inputCheckout)
+		assert.Equal(t, float64(20), output.fare)
+	})
+
+	t.Run("Deve calcular tarifa para shopping", func(t *testing.T) {
+		plate := generatedCarPlate(t)
+		db := connectionDb(t)
+		parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
+		checkin := NewCheckin(parkingTicketRepo)
+		inputCheckin := Input{
+			plate:       plate,
+			checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
+			location:    "shopping",
+		}
+		checkin.Execute(inputCheckin)
+		checkout := NewCheckout(parkingTicketRepo)
+		inputCheckout := InputCheckout{
+			plate:        plate,
+			checkoutDate: time.Date(2025, 07, 15, 15, 0, 0, 0, time.Local),
+		}
+		output := checkout.Execute(inputCheckout)
+		assert.Equal(t, float64(30), output.fare)
+	})
+
+	t.Run("Deve calcular tarifa para a praia", func(t *testing.T) {
+		plate := generatedCarPlate(t)
+		db := connectionDb(t)
+		parkingTicketRepo := NewParkingTicketRepositoryDatabase(db)
+		checkin := NewCheckin(parkingTicketRepo)
+		inputCheckin := Input{
+			plate:       plate,
+			checkinDate: time.Date(2025, 07, 15, 10, 0, 0, 0, time.Local),
+			location:    "beach",
+		}
+		checkin.Execute(inputCheckin)
+		checkout := NewCheckout(parkingTicketRepo)
+		inputCheckout := InputCheckout{
+			plate:        plate,
+			checkoutDate: time.Date(2025, 07, 15, 15, 0, 0, 0, time.Local),
+		}
+		output := checkout.Execute(inputCheckout)
+		assert.Equal(t, float64(10), output.fare)
+	})
 }
