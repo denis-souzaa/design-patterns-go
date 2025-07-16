6bed740bc863f8c65c30e25432749f4dce92bed2
Denis Souza <denyssouza1@hotmail.com>
2025-07-16 12:02:42 -0300

    refatorando testes do pattern command

--- command/bank_account_test.go
+++ command/bank_account_test.go
@@ -7,23 +7,25 @@ import (
 )
 
 func TestMustMakeTransaction(t *testing.T) {
-	from := &BankAccount{}
-	to := &BankAccount{}
-	assert.Equal(t, float64(0), from.Balance())
-	assert.Equal(t, float64(0), to.Balance())
-	from.Debit(100)
-	to.Credit(100)
-	assert.Equal(t, float64(-100), from.Balance())
-	assert.Equal(t, float64(100), to.Balance())
-}
+	t.Run("Deve fazer uma transferencia bancária", func(t *testing.T) {
+		from := &BankAccount{}
+		to := &BankAccount{}
+		assert.Equal(t, float64(0), from.Balance())
+		assert.Equal(t, float64(0), to.Balance())
+		from.Debit(100)
+		to.Credit(100)
+		assert.Equal(t, float64(-100), from.Balance())
+		assert.Equal(t, float64(100), to.Balance())
+	})
 
-func TestMustMakeTransactionWithCommand(t *testing.T) {
-	from := &BankAccount{}
-	to := &BankAccount{}
-	assert.Equal(t, float64(0), from.Balance())
-	assert.Equal(t, float64(0), to.Balance())
-	transferCommand := NewTransferCommand(from, to, 100)
-	transferCommand.Execute()
-	assert.Equal(t, float64(-100), from.Balance())
-	assert.Equal(t, float64(100), to.Balance())
+	t.Run("Deve fazer um transferencia bancária usando command", func(t *testing.T) {
+		from := &BankAccount{}
+		to := &BankAccount{}
+		assert.Equal(t, float64(0), from.Balance())
+		assert.Equal(t, float64(0), to.Balance())
+		transferCommand := NewTransferCommand(from, to, 100)
+		transferCommand.Execute()
+		assert.Equal(t, float64(-100), from.Balance())
+		assert.Equal(t, float64(100), to.Balance())
+	})
 }
